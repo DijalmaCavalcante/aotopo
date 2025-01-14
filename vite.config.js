bfb48e1e5a5fb358d7476da21c36b4d35028b12b
dijalmacavalcante <dijalma0412@gmail.com>
2025-01-14 00:57:41 -0300

    ci: configuracoes iniciais

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 3000, // Porta padrão, pode ser alterada se necessário
+  },
+  // Outras configurações do Vite
+});
