ca4174538678257166f82acd2add685b2efcebf5
dijalmacavalcante <dijalma0412@gmail.com>
2025-03-12 20:26:15 -0300

    ci: teste

--- loadpages.js
+++ loadpages.js
@@ -29,5 +29,4 @@ document.addEventListener("DOMContentLoaded", function() {
   loadpages("members", "/src/members/members.html");
   loadpages("pictures", "/src/pictures/pictures.html");
   loadpages("footer", "/src/footer/footer.html");
-  loadpages("merchandising", "/src/merchandising/merchandising.html");
 });
