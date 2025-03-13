5ac509d4926f835338937302bd17e8bc3ed1e88e
dijalmacavalcante <dijalma0412@gmail.com>
2025-03-12 21:26:00 -0300

    ci: teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,17 @@
 import { defineConfig } from 'vite';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import path from 'path';
 
 export default defineConfig({
   base: '/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',  
+    assetsDir: 'assets',
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        loadpages: path.resolve(__dirname, 'loadpages.js') // Garante que loadpages.js será incluído
-      },
+      input: 'index.html',
       output: {
-        entryFileNames: '[name].js',  // Remove hash dos arquivos JS
-        chunkFileNames: '[name].js',  // Remove hash dos chunks
-        assetFileNames: '[name][extname]' // Remove hash dos assets (CSS, imagens, etc.)
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name][extname]',
       }
     },
     modulePreload: { polyfill: true }
@@ -24,13 +20,8 @@ export default defineConfig({
   plugins: [
     viteStaticCopy({
       targets: [
-        { src: 'src/banner/banner.html', dest: '' }, 
-        { src: 'src/footer/footer.html', dest: '' },
-        { src: 'src/members/members.html', dest: '' },
-        { src: 'src/infomusic/infomusic.html', dest: '' },
-        { src: 'src/pictures/pictures.html', dest: '' }
+        { src: 'src/**/*.html', dest: 'src' }  // Copia os arquivos HTML para a pasta dist/src/
       ]
     })
-    
   ]
 });
