4ebf8110de3ca290bc70ce6849ed59a6eeaf2a60
dijalmacavalcante <dijalma0412@gmail.com>
2026-04-20 12:07:15 -0300

    chore: movendo hero para visualizacao separada para melhor compreensao

--- src/pages/home/component.jsx
+++ src/pages/home/component.jsx
@@ -1,37 +1,12 @@
 
 import Header from '@components/header';
-import Hero from '@components/hero';
+import Hero from './hero';
 
 function Home() {
-
   return (
     <>
-      <Header>
-      </Header>
-      <Hero
-        title={
-          <>AO TOPO<span style={{ color: "var(--color-orange)" }}>!</span></>
-        }
-        subtitle="De Guarulhos pro mund"
-        buttons={[
-          {
-            label: "Ouça Fazer Valer",
-            variant: "filled",
-            href: "https://open.spotify.com/",
-          },
-          {
-            label: "Conheça a Banda",
-            variant: "outlined",
-            href: "/sobre",
-          },
-        ]}
-        socialLinks={[
-          { label: "Instagram", href: "https://instagram.com/aotopo", },
-          { label: "YouTube",   href: "https://youtube.com/aotopo",   },
-          { label: "TikTok",    href: "https://tiktok.com/@aotopo",   },
-        ]}
-        scrollTargetId="sobre"
-      />
+      <Header />
+      <Hero />
     </>
   )
 }
